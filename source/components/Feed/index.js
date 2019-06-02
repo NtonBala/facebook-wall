f4ce31d2334a65a2e98c0cb77b0f07501cf44e43
Anton Balashov <anton.b@coaxsoft.com>
2019-06-02 20:29:29 +0300

    [L-1] Create posts feed

---
+++ source/components/Feed/index.js
@@ -0,0 +1,16 @@
+//Describes posts feed
+import React, { Component } from 'react';
+
+import { Composer } from 'components/Composer';
+import { Post } from 'components/Post';
+
+export class Feed extends Component {
+    render() {
+        return (
+            <section>
+                <Composer/>
+                <Post/>
+            </section>
+        );
+    }
+}
