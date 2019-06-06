73af89b58c69013246c78885347e077eb0515f3a
Anton Balashov <anton.b@coaxsoft.com>
2019-06-06 23:17:40 +0300

    [H-2] Update Post behavior

--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -1,35 +1,29 @@
 // Core
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import moment from 'moment';
+
+// Components
+import { Consumer } from 'components/HOC/withProfile';
 
 // Instruments
 import Styles from './styles.m.css';
 
 export class Post extends Component {
-    static propTypes = {
-        avatar:               PropTypes.string,
-        currentUserFirstName: PropTypes.string,
-        currentUserLastName:  PropTypes.string,
-    }
-
     render() {
-        const {
-            avatar,
-            currentUserFirstName,
-            currentUserLastName,
-        } = this.props;
-
         return (
-            <section className = { Styles.post }>
-                <img src = { avatar } />
-                <a>
-                    { `${currentUserFirstName} ${
-                        currentUserLastName}` }
-                </a>
-                <time>{moment().format('MMMM D h:mm:ss a')}</time>
-                <p>Howdy!</p>
-            </section>
+            <Consumer>
+                {(context) => (
+                    <section className = { Styles.post }>
+                        <img src = { context.avatar } />
+                        <a>
+                            { `${context.currentUserFirstName} ${
+                                context.currentUserLastName}` }
+                        </a>
+                        <time>{moment().format('MMMM D h:mm:ss a')}</time>
+                        <p>Howdy!</p>
+                    </section>
+                )}
+            </Consumer>
         );
     }
 }
