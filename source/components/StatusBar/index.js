9886320492654840e508682acd0e5fe25e1c0346
Anton Balashov <anton.b@coaxsoft.com>
2019-06-23 21:39:07 +0300

    [H-5] Update StatusBar behavior

--- source/components/StatusBar/index.js
+++ source/components/StatusBar/index.js
@@ -2,28 +2,34 @@
 
 //Core
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 //Components
-import { Consumer } from 'components/HOC/withProfile';
+import { withProfile } from 'components/HOC/withProfile';
 
 //Instruments
 import Styles from './styles.m.css';
 
+@withProfile
 export class StatusBar extends Component {
+    static propTypes = {
+        avatar:               PropTypes.string.isRequired,
+        currentUserFirstName: PropTypes.string.isRequired,
+        currentUserLastName:  PropTypes.string.isRequired,
+    };
+
     render() {
+        const { avatar, currentUserFirstName, currentUserLastName } = this.props;
+
         return (
-            <Consumer>
-                {(context) => (
-                    <section className = { Styles.statusBar }>
-                        <button>
-                            <img src = { context.avatar }/>
-                            <span>{ context.currentUserFirstName }</span>
-                                &#160;
-                            <span>{ context.currentUserLastName }</span>
-                        </button>
-                    </section>
-                )}
-            </Consumer>
+            <section className = { Styles.statusBar }>
+                <button>
+                    <img src = { avatar }/>
+                    <span>{ currentUserFirstName }</span>
+                        &#160;
+                    <span>{ currentUserLastName }</span>
+                </button>
+            </section>
         );
     }
 }
