0162350b921986c5193adab6d28a49cccef6baea
Anton Balashov <anton.b@coaxsoft.com>
2019-06-23 21:35:11 +0300

    [H-5] Update Post behavior

--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -4,17 +4,21 @@ import moment from 'moment';
 import PropTypes from 'prop-types';
 
 // Components
-import { Consumer } from 'components/HOC/withProfile';
+import { withProfile } from 'components/HOC/withProfile';
 
 // Instruments
 import Styles from './styles.m.css';
 
+@withProfile
 export class Post extends Component {
     static propTypes = {
-        id:          PropTypes.string.isRequired,
-        comment:     PropTypes.string.isRequired,
-        created:     PropTypes.object.isRequired,
-        _deletePost: PropTypes.func.isRequired,
+        id:                   PropTypes.string.isRequired,
+        comment:              PropTypes.string.isRequired,
+        created:              PropTypes.object.isRequired,
+        _deletePost:          PropTypes.func.isRequired,
+        avatar:               PropTypes.string.isRequired,
+        currentUserFirstName: PropTypes.string.isRequired,
+        currentUserLastName:  PropTypes.string.isRequired,
     };
 
     _handleDelete = () => {
@@ -25,25 +29,22 @@ export class Post extends Component {
 
     render() {
         const { comment, created } = this.props;
+        const { avatar, currentUserFirstName, currentUserLastName } = this.props;
 
         return (
-            <Consumer>
-                {(context) => (
-                    <section className = { Styles.post }>
-                        <img src = { context.avatar } />
-                        <a>
-                            { `${context.currentUserFirstName} ${
-                                context.currentUserLastName}` }
-                        </a>
-                        <time>{moment.unix(created).format('MMMM D h:mm:ss a')}</time>
-                        <p>{ comment }</p>
-                        <button
-                            className = 'cross'
-                            onClick = { this._handleDelete }
-                        />
-                    </section>
-                )}
-            </Consumer>
+            <section className = { Styles.post }>
+                <img src = { avatar } />
+                <a>
+                    { `${currentUserFirstName} ${
+                        currentUserLastName}` }
+                </a>
+                <time>{moment.unix(created).format('MMMM D h:mm:ss a')}</time>
+                <p>{ comment }</p>
+                <button
+                    className = 'cross'
+                    onClick = { this._handleDelete }
+                />
+            </section>
         );
     }
 }
