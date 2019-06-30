a711a77e35681272bb7cbbd20528aedff42255e4
Anton Balashov <anton.b@coaxsoft.com>
2019-06-30 23:23:09 +0300

    [L-8] Update StatusBar behavior with online/offline status

--- source/components/StatusBar/index.js
+++ source/components/StatusBar/index.js
@@ -3,12 +3,14 @@
 //Core
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 
 //Components
 import { withProfile } from 'components/HOC/withProfile';
 
 //Instruments
 import Styles from './styles.m.css';
+import { socket } from 'socket/init';
 
 @withProfile
 export class StatusBar extends Component {
@@ -18,11 +20,49 @@ export class StatusBar extends Component {
         currentUserLastName:  PropTypes.string.isRequired,
     };
 
+    state = {
+        online: false,
+    }
+
+    componentDidMount() {
+        socket.on('connect', () => {
+            this.setState(() => ({
+                online: true,
+            }));
+        });
+
+        socket.on('disconnect', () => {
+            this.setState(() => ({
+                online: false,
+            }));
+        });
+    }
+
+    componentWillUnmount() {
+        socket.removeListener('connect');
+        socket.removeListener('disconnect');
+    }
+
     render() {
-        const { avatar, currentUserFirstName, currentUserLastName } = this.props;
+        const {
+            avatar,
+            currentUserFirstName,
+            currentUserLastName,
+        } = this.props;
+        const { online } = this.state;
+
+        const statusStyle = cx(Styles.status, {
+            [ Styles.online ]:  online,
+            [ Styles.offline ]: !online,
+        });
+        const statusMessage = online ? 'Online' : 'Offline';
 
         return (
             <section className = { Styles.statusBar }>
+                <div className = { statusStyle }>
+                    <div>{ statusMessage }</div>
+                    <span/>
+                </div>
                 <button>
                     <img src = { avatar }/>
                     <span>{ currentUserFirstName }</span>
