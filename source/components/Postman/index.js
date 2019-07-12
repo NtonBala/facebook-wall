a45e0c260ab4a0706f0ab6d02c0eaa6ad5c2060f
Anton Balashov <anton.b@coaxsoft.com>
2019-07-12 17:48:10 +0300

    [H-7] Refactor Postman component

--- source/components/Postman/index.js
+++ source/components/Postman/index.js
@@ -10,27 +10,20 @@ import { withProfile } from 'components/HOC/withProfile';
 
 let Postman = ({ avatar, currentUserFirstName }) => {
     const animatePostmanEnter = (postman) => {
-        fromTo(
-            postman,
-            1,
-            { x: 280 },
-            {
-                x:          0,
-                onComplete: () => {
-                    setTimeout(() => {
-                        fromTo(postman, 1, { x: 0 }, { x: 280 });
-                    }, 4000);
-                },
-            },
-        );
+        fromTo(postman, 1, { x: 280 }, { x: 0 });
+    };
+
+    const animatePostmanLeave = (postman) => {
+        fromTo(postman, 1, { x: 0 }, { x: 280 });
     };
 
     return (
         <Transition
             appear
             in
-            timeout = { 1000 }
-            onEnter = { animatePostmanEnter }>
+            timeout = { 4000 }
+            onEntered = { animatePostmanLeave }
+            onEntering = { animatePostmanEnter }>
             <section className = { Styles.postman }>
                 <img src = { avatar } />
                 <span>Welcome online, { currentUserFirstName }</span>
