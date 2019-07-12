17089883484531d52f5e6a914bf3af9a0ea0a9be
Anton Balashov <anton.b@coaxsoft.com>
2019-07-12 21:51:04 +0300

    [H-7] Provided changes after review

--- source/components/Postman/index.js
+++ source/components/Postman/index.js
@@ -1,34 +1,17 @@
 // Core
 import React from 'react';
 import { string } from 'prop-types';
-import { Transition } from 'react-transition-group';
-import { fromTo } from 'gsap';
 
 // Instruments
 import Styles from './styles.m.css';
 import { withProfile } from 'components/HOC/withProfile';
 
 let Postman = ({ avatar, currentUserFirstName }) => {
-    const animatePostmanEnter = (postman) => {
-        fromTo(postman, 1, { x: 280 }, { x: 0 });
-    };
-
-    const animatePostmanLeave = (postman) => {
-        fromTo(postman, 1, { x: 0 }, { x: 280 });
-    };
-
     return (
-        <Transition
-            appear
-            in
-            timeout = { 4000 }
-            onEntered = { animatePostmanLeave }
-            onEntering = { animatePostmanEnter }>
-            <section className = { Styles.postman }>
-                <img src = { avatar } />
-                <span>Welcome online, { currentUserFirstName }</span>
-            </section>
-        </Transition>
+        <section className = { Styles.postman }>
+            <img src = { avatar } />
+            <span>Welcome online, { currentUserFirstName }</span>
+        </section>
     );
 };
 
