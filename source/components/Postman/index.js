dc984cefb80dec8d40e4a243758eb2bcdadf2ded
Anton Balashov <anton.b@coaxsoft.com>
2019-07-12 17:31:41 +0300

    [H-7] Animate Postman mounting & disappearing after mounting

--- source/components/Postman/index.js
+++ source/components/Postman/index.js
@@ -1,17 +1,41 @@
 // Core
 import React from 'react';
 import { string } from 'prop-types';
+import { Transition } from 'react-transition-group';
+import { fromTo } from 'gsap';
 
 // Instruments
 import Styles from './styles.m.css';
 import { withProfile } from 'components/HOC/withProfile';
 
 let Postman = ({ avatar, currentUserFirstName }) => {
+    const animatePostmanEnter = (postman) => {
+        fromTo(
+            postman,
+            1,
+            { x: 280 },
+            {
+                x:          0,
+                onComplete: () => {
+                    setTimeout(() => {
+                        fromTo(postman, 1, { x: 0 }, { x: 280 });
+                    }, 4000);
+                },
+            },
+        );
+    };
+
     return (
-        <section className = { Styles.postman }>
-            <img src = { avatar } />
-            <span>Welcome online, { currentUserFirstName }</span>
-        </section>
+        <Transition
+            appear
+            in
+            timeout = { 1000 }
+            onEnter = { animatePostmanEnter }>
+            <section className = { Styles.postman }>
+                <img src = { avatar } />
+                <span>Welcome online, { currentUserFirstName }</span>
+            </section>
+        </Transition>
     );
 };
 
