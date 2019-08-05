4cfafe8b6793ffe8dd23869da7075ead24dea078
Anton Balashov <balashov.anton.1983@gmail.com>
2019-08-05 17:27:31 +0300

    [L-11] Test getUniqueID function

--- source/instruments/index.test.js
+++ source/instruments/index.test.js
@@ -1,5 +1,5 @@
 // Core
-import { sum, delay } from './';
+import { sum, delay, getUniqueID } from './';
 
 describe('instruments:', () => {
     test('sum function should be a function', () => {
@@ -21,5 +21,19 @@ describe('instruments:', () => {
 
     test('delay function should return a resolved promise', async () => {
         await expect(delay(5000)).resolves.toBe('A resolved promise');
+    });
+
+    test('getUniqueID function should be a function', () => {
+        expect(getUniqueID).toBeInstanceOf(Function);
+    });
+
+    test('getUniqueID function should throw, when called with non-number type as an argument', () => {
+        expect(() => getUniqueID('argument')).toThrow();
+    });
+
+    test('getUniqueID function should produce a string of a desired given length', () => {
+        expect(typeof getUniqueID()).toBe('string');
+        expect(getUniqueID(5)).toHaveLength(5);
+        expect(getUniqueID(13)).toHaveLength(13);
     });
 });
